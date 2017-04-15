5154d0eaa126f0066fe731e22a383548c3709086
lebedeanastasija <lebedeanastasija@yandex.by>
2017-04-15 12:55:54 +0300

    Draw hierachy of input object

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var numbers,
-	numbersCount = 20;	
+var distancesMatrix = [[0, null, null, null, null], 
+				 	   [null, 0, null, null, null],
+				 	   [null, null, 0, null, null],
+				 	   [null, null, null, 0, null],
+				 	   [null, null, null, null, 0]],
+
+	objectsCount = 5,
+	distancesCount,
+	distancesInfo = [],
+	distancesValues,
+	usedObjects = [],
+	removeOnNextStep = [];
 
 var svgContainer,
 	axisLength = 450,
-	maxAxisValue = 10,
+	maxAxisValue = 20,
 	startPoint = {
 		x: 50,
 		y: 500
@@ -32,13 +42,140 @@ function onLoad (){
 
 	scale = getScale();
 	drawCoordinateAxes(axisLength, {x0: 50, y0: 500}, scale);		
-	drawPoints(trainingPoints, colors[0], 4);
 
-	console.log(separatingFunction);
+	distancesCount = getDistancesCount(objectsCount);
+	distancesValues = generateNumbers(distancesCount, {min: 1, max: 20});
+	for(var i = 0; i < distancesValues.length; i++) {
 
-	numbers = generateNumbers(numbersCount, {min: 0, max: maxAxisValue});
-	console.log(numbers);
+	}
+	initializeDistances(objectsCount);
+	console.log("Distances info", distancesInfo);
+	generateMinTree();
 }
+
+function getDistancesCount(objectsCount) {
+	return objectsCount * (objectsCount - 1) / 2;
+}
+
+function initializeDistances(objectsCount) {
+	var tempDistances = Array.from(distancesValues);
+	for(var i = 0; i < objectsCount; i++) {
+		for(var j = i; j < objectsCount; j++) {
+			if(i !== j) {
+				var temp = tempDistances.shift();
+				distancesMatrix[i][j] = temp;
+				distancesMatrix[j][i] = temp;
+				var info = {
+					value: temp,
+					indexes: [i, j]
+				}
+				distancesInfo.push(info);
+			}
+		}
+	}
+}
+
+function generateMinTree() {
+	while(distancesValues.length) {
+		var point1 = null,
+			point2 = null;
+
+		removeOnNextStep.forEach(index => {
+			distancesInfo.splice(index, 1);
+			distancesValues.splice(index, 1);
+		})
+		if(distancesValues.length < 1) {
+			return;
+		}
+		removeOnNextStep = [];
+		var minValue = Math.min.apply(null, distancesValues);
+		console.log(minValue);
+		console.log("distances", distancesInfo.length);
+		var tempArray = Array.from(distancesInfo);
+		console.log(tempArray);
+		console.log("already used", usedObjects.length);
+		tempArray = Array.from(usedObjects);
+		console.log(tempArray);
+		var minIndex = distancesValues.indexOf(minValue);
+		distancesValues.splice(minIndex, 1);
+
+
+		var distanceInfo = distancesInfo.splice(minIndex, 1)[0];
+		
+		if(usedObjects.indexOf(distanceInfo.indexes[0]) < 0 ) {
+			usedObjects.push(distanceInfo.indexes[0]);
+			point1 = {x: usedObjects.length * 2, y: distanceInfo.value}
+			drawPoint(point1, colors[3], 5);
+		
+			svgContainer.append("text")
+    		.attr("x", usedObjects.length * 2 * scale + startPoint.x - 5)
+    		.attr("y",startPoint.y - (distanceInfo.value * scale) - 10)
+    		.attr("dy", ".35em")
+    		.text("x" + distanceInfo.indexes[0]);
+
+		} else {
+			var firstIndexArray = [];
+			distancesInfo.forEach( (distance, index) => {			
+				if(distance.indexes[0] === distanceInfo.indexes[0] ||
+				   distance.indexes[1] === distanceInfo.indexes[0] ) {
+					firstIndexArray.push({distance: distance.value, index: index});
+				}				
+			});
+
+			var firstMinInfo = findMinDistance(firstIndexArray);
+			distancesInfo.forEach((distance, index) => {
+				if(index !== firstMinInfo.index && distance.indexes.indexOf(distanceInfo[1]) < 0) {
+					removeOnNextStep.push(index);
+				}
+			})
+	
+		}
+		if(usedObjects.indexOf(distanceInfo.indexes[1]) < 0) {
+			usedObjects.push(distanceInfo.indexes[1]);
+			point2 = {x: usedObjects.length * 2, y: distanceInfo.value};
+			drawPoint(point2, colors[3], 5);
+			svgContainer.append("text")
+    		.attr("x", usedObjects.length * 2 * scale + startPoint.x - 5)
+    		.attr("y",startPoint.y - (distanceInfo.value * scale) - 10)
+    		.attr("dy", ".35em")
+    		.text("x" + distanceInfo.indexes[1]);
+
+		} else {
+			var secondIndexArray = [];
+			distancesInfo.forEach( (distance, index) => {
+				if(distance.indexes[0] === distanceInfo.indexes[1] ||
+				   distance.indexes[1] === distanceInfo.indexes[1] ) {
+					secondIndexArray.push({distance: distance.value, index: index});
+				}
+			});
+			var secondMinInfo = findMinDistance(secondIndexArray);
+			distancesInfo.forEach((distance, index) => {
+				if(index !== secondMinInfo.index && distance.indexes.indexOf(distanceInfo[0]) < 0) {
+					removeOnNextStep.push(index);
+				}
+			})
+		}
+		if(point1 && point2) {
+			drawLine(point1, point2, colors[6], 4);
+		}
+	}		
+}
+
+function findMinDistance(objectsArray) {
+	var minIndex = -1,
+	    minDistance = 100;
+	objectsArray.forEach((objectItem) => {
+		if(objectItem.distance < minDistance) {
+			minDistance = objectItem.distance;
+			minIndex = objectItem.index;
+		}
+	});
+	return {
+		distance: minDistance, 
+		index: minIndex
+	}
+}
+
 
 ////////////////////////////////////////////
 ////////////GENERAL FUNCTIONALITY///////////
@@ -46,8 +183,12 @@ function onLoad (){
 
 function generateNumbers(count, range) {
 	var numbers = []
-	for(var i = 0; i < count; i++) {		
-		numbers.push(randomInRange(range.min, range.max));		
+	for(var i = 0; i < count; i++) {	
+		var temp = randomInRange(range.min, range.max);
+		while(numbers.indexOf(temp) > -1){
+			temp = randomInRange(range.min, range.max);
+		}
+		numbers.push(temp);			
 	}
 	return numbers;
 }
@@ -122,25 +263,25 @@ function drawPoints(points, color, width, connect) {
 	} else {
 		for(var i = 1; i < points.length; i++) {
 			drawPoint(points[i], color, width);
-			drawLine(pints[i-1], functionResults[i], color, width);
+			drawLine(points[i-1], functionResults[i], color, width);
 		}
 	}		
 }
 
 function drawPoint(point, color, size) {
 	svgContainer.append("circle")
-	.attr("cx", point.x * (scale/2) + startPoint.x + axisLength/2 + 20)
-	.attr("cy", startPoint.y - (point.y * (scale/2)) - axisLength/2 ) 
+	.attr("cx", point.x * (scale) + startPoint.x)
+	.attr("cy", startPoint.y - (point.y * (scale))) 
 	.attr("r", size || 1)
 	.style("fill", color);
 }
 
 function drawLine(point1, point2, color, width) {	
 	svgContainer.append("line")
-    .attr("x1", point1.x * (scale/2) + startPoint.x + axisLength/2 + 20)
-    .attr("y1", startPoint.y - (point1.y * (scale/2)) - axisLength/2)
-    .attr("x2", point2.x * (scale/2) + startPoint.x + axisLength/2 + 20)
-    .attr("y2", startPoint.y - (point2.y * (scale/2)) - axisLength/2)
+    .attr("x1", point1.x * scale + startPoint.x)
+    .attr("y1", startPoint.y - point1.y * scale)
+    .attr("x2", point2.x * scale + startPoint.x)
+    .attr("y2", startPoint.y - point2.y * scale)
     .attr("stroke-width", width)
     .attr("stroke", color);
 }
